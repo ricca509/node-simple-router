31d95941141be661b43e92a66f859eb8c685d6a9
Riccardo Coppola <Riccardo.Coppola@thetrainline.com>
2018-09-05 00:13:36 +0200

    Refactors router into main function and updates README

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 
 ```
-const { router } = require('node-router');
-const { home, news, handler404 } = require('./handlers');
+const { router } = require('node-simple-router');
+const { home, search, handler404 } = require('./handlers');
 
-router.get(/\/api\/news.*/, news);
+router.get(/\/api\/search.*/, news);
 router.get(/\/app.*/, home);
+// Catchall route
 router.get(handler404);
 
+// Express
+const app = express();
 app.use(router);
 
+// Plain node http/https
+const server = http.createServer(router);
 ```
--- src/index.js
+++ src/index.js
@@ -17,37 +17,42 @@ const supportedHttpMethods = [
   'TRACE',
   'PATCH'
 ];
-const routes = new Map();
 
-const matcher = (req, res) => {
-  var { pathname } = url.parse(req.url);
-  for (let [method, options] of routes.entries()) {
-    let { path, handler } = options;
+const router = () => {
+  const routes = new Map();
 
-    if (method === req.method) {
-      const match = path.test && path.test(pathname);
-      if (match === true) {
-        logGreen(`found match "${path}" for "${req.url}"`);
-        return handler(req, res);
+  const matcher = (req, res) => {
+    var { pathname } = url.parse(req.url);
+    for (let [method, options] of routes.entries()) {
+      let { path, handler } = options;
+
+      if (method === req.method) {
+        const match = path.test && path.test(pathname);
+        if (match === true) {
+          logGreen(`found match "${path}" for "${req.url}"`);
+          return handler(req, res);
+        }
       }
     }
-  }
 
-  logRed(`no match found for "${req.url}"`);
-};
-
-const methods = supportedHttpMethods.forEach(method => {
-  matcher[method.toLowerCase()] = (path, handler) => {
-    if (!handler) {
-      handler = path;
-      path = /.*/;
-    }
-
-    routes.set(method, { path, handler });
+    logRed(`no match found for "${req.url}"`);
   };
-});
+
+  const methods = supportedHttpMethods.forEach(method => {
+    matcher[method.toLowerCase()] = (path, handler) => {
+      if (!handler) {
+        handler = path;
+        path = /.*/;
+      }
+
+      routes.set(method, { path, handler });
+    };
+  });
+
+  return matcher;
+}
 
 module.exports = {
-  router: matcher,
+  router: router(),
   supportedHttpMethods
 };
